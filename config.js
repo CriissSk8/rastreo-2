13f2b0ce0b2be784411f6b4ba63b38a92a0d9f6a
CriissSk8 <esqueree22@gmail.com>
2025-11-29 01:50:06 -0500

    Update config.js

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ const CONFIG = {
     // 2. Regístrate gratis
     // 3. Copia tu API key
     // 4. Pégala aquí:
-    JSONBIN_API_KEY: '$2a$10$TU_API_KEY_AQUI',
+    JSONBIN_API_KEY: '$2a$10$s2Isg/KN8JgTdp02jDV.3evi1W941I/1fnHVN9zKSzxxCFwscdsqG',
     
     // Después de crear tu primera bin, pega el ID aquí:
-    JSONBIN_BIN_ID: 'TU_BIN_ID_AQUI',
+    JSONBIN_BIN_ID: '692a96cfd0ea881f40074a0a',
     
     // Alternativamente, usa un webhook de Discord, Telegram, o email
     // Para Discord: Crea un webhook en tu servidor y pega la URL aquí
@@ -27,3 +27,4 @@ const CONFIG = {
 if (typeof module !== 'undefined' && module.exports) {
     module.exports = CONFIG;
 }
+
